679608fc49666b7cfad559357852dbce02b6dce5
Michael1119 <michaelonl@hotmail.com>
2022-11-10 17:08:32 +0800

    initial commit

--- DC011407/exercise5/exercise5.cpp
+++ DC011407/exercise5/exercise5.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
+#include <vector>
 #include <stdexcept>
 
 using namespace std;
 
-template <typename T>
-class stack {
-private:
-    size_t capacity = 20;
-    vector<T> mydata;
-public:
-    stack() {mydata.reserve(capacity);}
-    ~stack() {}
-    T top() {return mydata.at(mydata.size() - 1);}
-    void pop() {
-        if (empty()) {
-            throw underflow_error("Stack underflowed!");
+namespace Michael {
+    template <typename T>
+    class stack {
+    private:
+        size_t capacity = 10;
+        vector<T> mydata;
+    public:
+        stack() {mydata.reserve(capacity);}
+        ~stack() {}
+        T top() {return mydata.at(mydata.size() - 1);}
+        void pop() {
+            if (empty()) {
+                throw underflow_error("Stack underflowed!");
+            }
+            else {
+                mydata.pop_back();
+            }
         }
-        else {
-            mydata.pop_back();
+        void push(T t) {
+            if (full()) {
+                throw overflow_error("Stack overflowed!");
+            }
+            else {
+                mydata.push_back(t);
+            }
         }
-    }
-    void push() {
-        if (full()) {
-            throw overflow_error("Stack overflowed!");
-        }
-        else {
-            mydata.push_back();
-        }
-    }
-    void clear() {mydata.clear();}
-    size_t size() {return mydata.size();}
-    bool full() const noexcept {return mydata.size() == capacity;}
-    bool empty() const noexcept {return mydata.size() == 0;}
-};
+        void clear() {mydata.clear();}
+        size_t size() {return mydata.size();}
+        bool full() const noexcept {return mydata.size() == capacity;}
+        bool empty() const noexcept {return mydata.size() == 0;}
+    };
+}
 
 int main() {
+    Michael::stack<int> s;
+    for (int i = 1; i <= 10;) {
+        s.push(i);
+        i+=1;
+    }
+    cout << s.size() << endl;
+    cout << s.full() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.clear();
+    cout << s.empty() << endl;
     return 0;
 }
