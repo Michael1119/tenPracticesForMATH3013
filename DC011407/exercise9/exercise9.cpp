22df36132837caf4e5d98bff50569f47b901a5fc
Michael1119 <michaelonl@hotmail.com>
2022-11-11 17:49:55 +0800

    initial commit

--- DC011407/exercise9/exercise9.cpp
+++ DC011407/exercise9/exercise9.cpp
@@ -1,24 +1,20 @@
 #include <map>
 #include <string>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
     map<string, unsigned long> phone_numbers = {{"Michael", 28111111}, {"Henry", 28222222}, {"Sam", 28333333}, {"Jay", 28444444}, {"Luiz", 28555555}, {"Cathy", 28666666}};
-    for (auto it = phone_numbers.begin(); it != phone_numbers.end(); ++it) {
-        cout << it->first << " : " << it->second << endl;
-    }
-    string key = "!@#$";
+    string key;
     cout << "Input the name: ";
     cin >> key;
-    auto iter = phone_numbers.find(key);
-    if (iter == phone_numbers.end()) {
+    auto it = phone_numbers.find(key);
+    if (it == phone_numbers.end()) {
         cout <<"You don't have " << key << "'s phone number" << endl;
     }
     else {
-        cout << key << "'s phone number is " << iter->second << endl;
+        cout << key << "'s phone number is " << it->second << endl;
     }
     return 0;
 }
